a6bcada467a64b9e2a0809f4768cc9b2ee5ed12e
John Carl Cueva <ninjadentist24@gmail.com>
2025-02-27 13:06:22 +0800

    added hero section

--- src/layouts/GuestLayout.js
+++ src/layouts/GuestLayout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, Navigate, Outlet } from 'react-router-dom'
+import { Navigate, NavLink, Outlet } from 'react-router-dom'
 import { useStateContext } from '../context/ContextProvider'
 import Logo from '../components/Logo';
 import ThemeToggleButton from '../components/ThemeToggleButton';
@@ -18,34 +18,65 @@ function GuestLayout() {
 
     return (
         <>
-            <nav className='w-full p-4 flex items-center justify-between text-light-foreground dark:text-dark-foreground bg-light-background dark:bg-dark-accent fixed'>
+            <nav className='w-full p-4 flex items-center justify-between text-light-foreground dark:text-dark-foreground bg-light-background dark:bg-dark-accent fixed z-50'>
                 <a href='/' className='flex items-center gap-4 text-black dark:text-white'>
                     <Logo width={"50px"} height={"50px"} />
                 </a>
                 <div className='md:flex lg:flex gap-6 font-semibold hidden'>
-                    <a href='/' className='hover:text-primary'>
-                        <span>Home</span>
-                    </a>
-                    <Link
-                        className='hover:text-primary' to={'/bill-inquiry'}>
-                        <span>Bill Inquiry</span>
-                    </Link>
-                    <Link
-                        className='hover:text-primary' to={'/biddings'}>
-                        <span>Biddings</span>
-                    </Link>
-                    <Link
-                        className='hover:text-primary' to={'/consumer-services'}>
-                        <span>Consumer Services</span>
-                    </Link>
-                    <Link
-                        className='hover:text-primary' to={'/about'}>
-                        <span>About</span>
-                    </Link>
-                    <Link
-                        className='hover:text-primary' to={'/contact'}>
-                        <span>Contact</span>
-                    </Link>
+                    <NavLink
+                        to={'/'}
+                    >
+                        {({isActive}) => (isActive ? 
+                            <span className='text-blue-700 dark:text-primary'>Home</span>
+                            :
+                            <span className='hover:text-blue-700 hover:dark:text-primary'>Home</span>
+                        )}
+                    </NavLink>
+                    <NavLink
+                        to={'/bill-inquiry'}
+                    >
+                        {({isActive}) => (isActive ? 
+                            <span className='text-blue-700 dark:text-primary'>Bill Inquiry</span>
+                            :
+                            <span className='hover:text-blue-700 hover:dark:text-primary'>Bill Inquiry</span>
+                        )}
+                    </NavLink>
+                    <NavLink
+                        to={'/biddings'}
+                    >
+                        {({isActive}) => (isActive ? 
+                            <span className='text-blue-700 dark:text-primary'>Biddings</span>
+                            :
+                            <span className='hover:text-blue-700 hover:dark:text-primary'>Biddings</span>
+                        )}
+                    </NavLink>
+                    <NavLink
+                        to={'/consumer-services'}
+                    >
+                        {({isActive}) => (isActive ? 
+                            <span className='text-blue-700 dark:text-primary'>Consumer Services</span>
+                            :
+                            <span className='hover:text-blue-700 hover:dark:text-primary'>Consumer Services</span>
+                        )}
+                    </NavLink>
+                    <NavLink
+                        to={'/about'}
+                    >
+                        {({isActive}) => (isActive ? 
+                            <span className='text-blue-700 dark:text-primary'>About</span>
+                            :
+                            <span className='hover:text-blue-700 hover:dark:text-primary'>About</span>
+                        )}
+                    </NavLink>
+                    <NavLink
+                        to={'/contact'}
+                    >
+                        {({isActive}) => (isActive ? 
+                            <span className='text-blue-700 dark:text-primary'>Contact</span>
+                            :
+                            <span className='hover:text-blue-700 hover:dark:text-primary'>Contact</span>
+                        )}
+                    </NavLink>
                 </div>
                 <div className='flex items-center gap-4'>
                     <ThemeToggleButton />
@@ -58,24 +89,72 @@ function GuestLayout() {
                 {dropdown &&
                     <div 
                         className='flex flex-col justify-center bg-light-background dark:bg-dark-accent text-light-foreground dark:text-dark-foreground md:hidden lg:hidden'>
-                    <a href='/' className='p-4 flex hover:text-primary' onClick={() => setDropDown(false)}>
-                        <span>Home</span>
-                    </a>
-                    <Link className='p-4 flex hover:text-primary' to={'/bill-inquiry'} onClick={() => setDropDown(false)}>
-                        <span>Bill Inquiry</span>
-                    </Link>
-                    <Link className='p-4 flex hover:text-primary' to={'/biddings'} onClick={() => setDropDown(false)}>
-                        <span>Biddings</span>
-                    </Link>
-                    <Link className='p-4 flex hover:text-primary' to={'/consumer-services'} onClick={() => setDropDown(false)}>
-                        <span>Consumer Services</span>
-                    </Link>
-                    <Link className='p-4 flex hover:text-primary' to={'/about'} onClick={() => setDropDown(false)}>
-                        <span>About</span>
-                    </Link>
-                    <Link className='p-4 flex hover:text-primary' to={'/contact'} onClick={() => setDropDown(false)}>
-                        <span>Contact</span>
-                    </Link>
+                    <NavLink
+                        className='p-4 flex'
+                        to={'/'}
+                        onClick={() => setDropDown(false)}
+                    >
+                        {({isActive}) => (isActive ? 
+                            <span className='text-blue-700 dark:text-primary'>Home</span>
+                            :
+                            <span className='hover:text-blue-700 hover:dark:text-primary'>Home</span>
+                        )}
+                    </NavLink>
+                    <NavLink
+                        className='p-4 flex'
+                        to={'/bill-inquiry'}
+                        onClick={() => setDropDown(false)}
+                    >
+                        {({isActive}) => (isActive ? 
+                            <span className='text-blue-700 dark:text-primary'>Bill Inquiry</span>
+                            :
+                            <span className='hover:text-blue-700 hover:dark:text-primary'>Bill Inquiry</span>
+                        )}
+                    </NavLink>
+                    <NavLink
+                        className='p-4 flex'
+                        to={'/biddings'}
+                        onClick={() => setDropDown(false)}
+                    >
+                        {({isActive}) => (isActive ? 
+                            <span className='text-blue-700 dark:text-primary'>Biddings</span>
+                            :
+                            <span className='hover:text-blue-700 hover:dark:text-primary'>Biddings</span>
+                        )}
+                    </NavLink>
+                    <NavLink
+                        className='p-4 flex'
+                        to={'/consumer-services'}
+                        onClick={() => setDropDown(false)}
+                    >
+                        {({isActive}) => (isActive ? 
+                            <span className='text-blue-700 dark:text-primary'>Consumer Services</span>
+                            :
+                            <span className='hover:text-blue-700 hover:dark:text-primary'>Consumer Services</span>
+                        )}
+                    </NavLink>
+                    <NavLink
+                        className='p-4 flex'
+                        to={'/about'}
+                        onClick={() => setDropDown(false)}
+                    >
+                        {({isActive}) => (isActive ? 
+                            <span className='text-blue-700 dark:text-primary'>About</span>
+                            :
+                            <span className='hover:text-blue-700 hover:dark:text-primary'>About</span>
+                        )}
+                    </NavLink>
+                    <NavLink
+                        className='p-4 flex'
+                        to={'/contact'}
+                        onClick={() => setDropDown(false)}
+                    >
+                        {({isActive}) => (isActive ? 
+                            <span className='text-blue-700 dark:text-primary'>Contact</span>
+                            :
+                            <span className='hover:text-blue-700 hover:dark:text-primary'>Contact</span>
+                        )}
+                    </NavLink>
                 </div>}
             </div>
             <div className='min-h-[100svh] bg-light-background dark:bg-dark-background text-light-foreground dark:text-dark-foreground'>
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,47 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Landing() {
   return (
-    <div>
-        Landing
+<section
+  className="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat"
+>
+  <div
+    className="absolute inset-0 bg-gray-900/75 md:from-gray-900/75 md:to-gray-900/0 md:bg-gradient-to-r lg:from-gray-900/md:from-gray-900/75 lg:to-gray-900/0 lg:bg-gradient-to-r"
+  ></div>
+
+  <div
+    className="relative mx-auto w-full px-4 lg:px-8 h-[80svh] flex items-center justify-center lg:justify-start"
+  >
+    <div className="max-w-xl text-center lg:text-start">
+      <h1 className="text-3xl font-extrabold text-white sm:text-5xl">
+        Powering Your Future 
+
+        <strong className="block font-extrabold text-primary"> with Reliable Energy Solutions. </strong>
+      </h1>
+
+      <p className="mt-4 w-full text-white sm:text-xl/relaxed">
+        We provide sustainable, efficient, and affordable electricity for homes and businesses.
+      </p>
+
+      <div className="mt-8 flex flex-wrap gap-4 text-center justify-center lg:justify-start">
+        <Link
+          to={'/'}
+          className="block w-full rounded bg-blue-700 px-12 py-3 text-sm font-medium text-white shadow-sm hover:bg-blue-800 focus:ring-3 focus:outline-hidden sm:w-auto"
+        >
+          Get Started
+        </Link>
+
+        <Link
+          to={'/consumer-services'}
+          className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-blue-700 shadow-sm hover:text-blue-800 focus:ring-3 focus:outline-hidden sm:w-auto"
+        >
+          Learn More
+        </Link>
+      </div>
     </div>
+  </div>
+</section>
   )
 }
 
